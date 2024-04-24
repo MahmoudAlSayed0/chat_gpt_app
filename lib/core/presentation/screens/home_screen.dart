6b15c7e2386f5141dfca35d6f84364a97fea3430
Mahmoud <mahmoudalsayed2504@gmail.com>
2024-04-25 01:07:10 +0200

    done splash

--- lib/core/presentation/screens/home_screen.dart
+++ lib/core/presentation/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +10,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:chat_gpt_app/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.delayed(
+    const Duration(seconds: 5),
+    () {
+      FlutterNativeSplash.remove();
+    },
+  );
   runApp(const MainApp());
 }
